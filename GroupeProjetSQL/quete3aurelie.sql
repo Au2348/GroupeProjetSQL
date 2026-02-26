7746a5f3b8dea1ae4f87d21c3e528a2b563afd9a
Au2348 <aurelie.m@tutamail.com>
2026-02-26 07:33:07 +0100

    quete 1 et 2: lydia, quete 3: aurelie

--- GroupeProjetSQL/quete3aurelie.sql
+++ GroupeProjetSQL/quete3aurelie.sql
@@ -1,12 +1,18 @@
-SELECT location.id, client.nom, client.prenom
-FROM location
-JOIN client ON location.id_client = client.id;
+-- QUETE 3, jointures, (qui,quelle voiture/date_debut(delocation),trajet dep/arr)
 
-SELECT location.id, vehicule.marque, vehicule.modele
+-- Requête 1 : Afficher la date de début de chaque location, ainsi que le nom et prénom du client associé.
+SELECT l.date_debut, c.nom, c.prenom
 FROM location
-JOIN vehicule ON location.id_vehicule = vehicule.id;
+JOIN client ON l.id_client = c.id_client;
 
-SELECT location.id, dep.nom AS depart, arr.nom AS arrivee
+-- Requête 2 : Afficher la date de début de chaque location, ainsi que la marque et le modèle du véhicule associé.
+SELECT location.date_debut, v.marque, v.modele
 FROM location
-JOIN station AS dep ON location.id_station_depart = dep.id
-JOIN station AS arr ON location.id_station_arrivee = arr.id;
+JOIN vehicule ON l.id_vehicule = v.id_vehicule;
+
+-- Requête 3 : Afficher la date de début de chaque location, ainsi que le nom de la station de départ et d'arrivée.
+-- joindre la table station 2 fois,1.depart,2.arrivee
+SELECT l.id_location, dep.nom AS depart, arr.nom AS arrivee
+FROM location
+JOIN station AS dep ON l.id_station_depart = dep.id_station
+JOIN station AS arr ON l.id_station_arrivee = arr.id_station;
